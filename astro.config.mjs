326783701fda40f0cb22c7d0794345f8d7245c5d
BaskLash <olivier.luethy@gmx.net>
2026-01-03 12:41:31 +0100

    Blogs part implemented -> not clickable yet

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from "@tailwindcss/vite";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black: "#000000",
+        white: "#ffffff",
+        gray: {
+          100: "#f3f4f6",
+          200: "#e5e7eb",
+          300: "#d1d5db",
+          400: "#9ca3af",
+          500: "#6b7280",
+          600: "#4b5563",
+          700: "#374151",
+          800: "#1f2937",
+          900: "#111827",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
